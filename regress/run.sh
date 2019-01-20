27642abb837a7123d8347240f0bd4ed74e23588e
Aaron LI <aly@aaronly.me>
2019-01-20 21:57:08 +0800

    regress: Add script to help run regress tests

---
+++ regress/run.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+MAKE="${MAKE:-make}"
+LOG="${LOG:-${PWD}/regress.log}"
+
+PROGS="ci co merge rcs rcsclean rcsdiff rcsmerge rlog"
+ENVS=""
+for p in ${PROGS}; do
+	ln -sv ../rcs ${p}
+	P=$(echo "${p}" | tr '[:lower:]' '[:upper:]')
+	ENVS="${ENVS} ${P}=${PWD}/${p}"
+done
+
+echo "MAKE: ${MAKE}"
+echo "ENVS: ${ENVS}"
+echo "LOG: ${LOG}"
+
+[ -f "${LOG}" ] && mv ${LOG} ${LOG}.old
+env ${ENVS} ${MAKE} regress REGRESS_LOG=${LOG}
+
+echo ""
+echo "===================== RESULTS ====================="
+cat ${LOG}
+
+rm -v ${PROGS}
